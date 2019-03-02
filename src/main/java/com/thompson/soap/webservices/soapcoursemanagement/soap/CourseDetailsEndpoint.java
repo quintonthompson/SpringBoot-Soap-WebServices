a2078b8e5b0bbcbe97e725b68b41b7af85e5a97c
Qthomp <qthompsn@gmail.com>
2019-03-02 11:24:32 -0600

    added endpoint for GetAllCourseDetails response

--- src/main/java/com/thompson/soap/webservices/soapcoursemanagement/soap/CourseDetailsEndpoint.java
+++ src/main/java/com/thompson/soap/webservices/soapcoursemanagement/soap/CourseDetailsEndpoint.java
@@ -1,8 +1,6 @@
 package com.thompson.soap.webservices.soapcoursemanagement.soap;
 
-import com.thompson.coursedetails.CourseDetails;
-import com.thompson.coursedetails.GetCourseDetailsRequest;
-import com.thompson.coursedetails.GetCourseDetailsResponse;
+import com.thompson.coursedetails.*;
 import com.thompson.soap.webservices.soapcoursemanagement.soap.bean.Course;
 import com.thompson.soap.webservices.soapcoursemanagement.soap.service.CourseDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +8,8 @@ import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import java.util.List;
 
 @Endpoint
 public class CourseDetailsEndpoint {
@@ -22,17 +22,40 @@ public class CourseDetailsEndpoint {
     public GetCourseDetailsResponse processCourseDetailsRequest(@RequestPayload GetCourseDetailsRequest request){
         Course course = service.findById(request.getId());
 
-        return mapCourse(course);
+        return mapCourseDetails(course);
     }
 
-    private GetCourseDetailsResponse mapCourse(Course course) {
+    private GetCourseDetailsResponse mapCourseDetails(Course course) {
         GetCourseDetailsResponse response = new GetCourseDetailsResponse();
+        response.setCourseDetails(mapCourse(course));
+        return response;
+    }
 
+    private CourseDetails mapCourse(Course course) {
         CourseDetails courseDetails = new CourseDetails();
         courseDetails.setId(course.getId());
         courseDetails.setName(course.getName());
         courseDetails.setDescription(course.getDescription());
-        response.setCourseDetails(courseDetails);
+        return courseDetails;
+    }
+
+    @PayloadRoot(namespace = "http://www.quintonthompson.com/courses",localPart = "GetAllCourseDetailsRequest")//if request has these names it will map to this method
+    @ResponsePayload //This method will contain the response payload
+    public GetAllCourseDetailsResponse processAllCourseDetailsRequest(@RequestPayload GetAllCourseDetailsRequest request){
+        List<Course> courses = service.findAllCourses();
+
+        return mapAllCourseDetails(courses);
+    }
+
+    private GetAllCourseDetailsResponse mapAllCourseDetails(List<Course> courses) {
+        GetAllCourseDetailsResponse response = new GetAllCourseDetailsResponse();
+
+        //GetAllCoursesResponse has a list because the max occurs are unbounded
+        //since response is now an array defined in GetAllCoursesResponse class we each map to response
+        for(Course course : courses){
+            CourseDetails mapCourse = mapCourse(course);
+            response.getCourseDetails().add(mapCourse);
+        }
         return response;
     }
 }
